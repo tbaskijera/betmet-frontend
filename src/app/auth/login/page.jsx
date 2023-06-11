ee07b361caf92a73c039b728b97a16e41b4216da
Martina Sirotic <martina.sirotic@student.uniri.hr>
2023-06-12 00:01:52 +0200

    BETMET-28 add style to login page

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -22,32 +22,49 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <div>
-        <h2>Login</h2>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="max-w-md w-full px-6 py-8 bg-white shadow-md">
+        <h2 className="text-2xl font-bold mb-8">Login</h2>
         <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="email">Email</label>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Email
+            </label>
             <input
               type="email"
               id="email"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-black"
               placeholder="Enter your email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
-          <div>
-            <label htmlFor="password">Password</label>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Password
+            </label>
             <input
               type="password"
               id="password"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-black-500 text-black"
               placeholder="Enter your password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <button type="submit">Login</button>
-          <div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:bg-blue-700"
+          >
+            Login
+          </button>
+          <div className="m-4 text-center text-red-500">
             <Link href="/auth/register">Not signed in? Register</Link>
           </div>
         </form>
