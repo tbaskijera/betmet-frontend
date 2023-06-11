7d44975aea608fecba048afee13fa2a8b9993fc3
Martina Sirotic <martina.sirotic@student.uniri.hr>
2023-06-11 23:57:01 +0200

    BETMET-28 update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,18 +6,23 @@ import useAuthStore from "@/zustand/AuthStore";
 const Navbar = () => {
   const { user, signOut } = useAuthStore();
   return (
-    <nav>
-      <div>
+    <nav className="bg-gray-900 py-4">
+      <div className="container mx-auto flex items-center justify-between">
         <Link href="/">Home</Link>
-        <ul>
+        <ul className="flex items-center space-x-4">
           <li>
             {user ? (
               <>
-                <span>{user.email}</span>
+                <span className="text-white">{user.email}</span>
                 <span>
                   <Link href="/betting-dashboard">Dashboard</Link>
                 </span>
-                <button onClick={signOut}>Sign Out</button>
+                <button
+                  className="text-white px-2 py-1 rounded-md bg-blue-500 hover:bg-blue-600"
+                  onClick={signOut}
+                >
+                  Sign Out
+                </button>
               </>
             ) : (
               <Link href="/auth/login">Login</Link>
