9552a746b6bb8d97406f6825ea276e7dc69efb5f
Martina Sirotic <martina.sirotic@student.uniri.hr>
2023-06-11 23:53:53 +0200

    BETMET-28 add colors in tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
-/** @type {import('tailwindcss').Config} */
+const { Colors } = require("./constants");
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primary: Colors.PRIMARY,
+        secondary: Colors.SECONDARY,
+        success: Colors.SUCCESS,
+        danger: Colors.DANGER,
+        warning: Colors.WARNING,
+        info: Colors.INFO,
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
