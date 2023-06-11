da5a88a5a2f6a00f2abbe65b7367b53967f538da
Martina Sirotic <martina.sirotic@student.uniri.hr>
2023-06-11 23:55:22 +0200

    BETMET-30 add placeholder data

---
+++ mockData.js
@@ -0,0 +1,31 @@
+export const matches = [
+  {
+    id: "match1",
+    team1: "Team A",
+    team2: "Team B",
+    odds: { team1: "2.0", team2: "1.5" },
+    date: "2023-06-15",
+  },
+  {
+    id: "match2",
+    team1: "Team C",
+    team2: "Team D",
+    odds: { team1: "1.8", team2: "2.2" },
+    date: "2023-06-16",
+  },
+  {
+    id: "match3",
+    team1: "Team E",
+    team2: "Team F",
+    odds: { team1: "1.6", team2: "2.5" },
+    date: "2023-06-17",
+  },
+  {
+    id: "match4",
+    team1: "Team G",
+    team2: "Team H",
+    odds: { team1: "2.3", team2: "1.3" },
+    date: "2023-06-18",
+  },
+  // Add more matches as needed
+];
